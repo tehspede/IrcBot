f3d1fe029914c6dfb39e6480520063c7b9aa52c3
Aleksi Gold <aleksi@kulta.us>
2016-03-22 12:39:12 +0200

    Add nickserv identification

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'cinch'
 require_relative 'plugins/plugin_manager'
+require_relative 'plugins/ban'
+require_relative 'plugins/nick'
+require_relative 'plugins/topic'
+require_relative 'plugins/tvdb'
+require_relative 'plugins/weather'
 require_relative 'migrations/initial'
+require 'cinch/plugins/identify'
 
 if !File.exist?('db/weather.db')
   InitialMigration.migrate(:up)
@@ -15,7 +21,11 @@ cinch = Cinch::Bot.new do
     config.channels = CHANNELS
     config.nick     = NICK
     config.user     = "SickGear"
-    config.plugins.plugins = [PluginManager]
+    config.plugins.plugins = [Cinch::Plugins::Identify, PluginManager, Tvdb, Ban, Nick, Topic, Weather]
+    config.plugins.options[Cinch::Plugins::Identify] = {
+        :password => NSPASS,
+        :type     => :nickserv,
+    }
   end
 end
 
